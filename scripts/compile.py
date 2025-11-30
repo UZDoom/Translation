4335011d2549e50649392d2b43b16daab6441061
Marcus Minhorst <marcus.minhorst@gmail.com>
2025-11-29 20:35:14 -0500

    Add some splits

--- scripts/compile.py
+++ scripts/compile.py
@@ -11,8 +11,7 @@ from pathlib import Path
 
 SOURCE_LANG = "en_US"
 
-def dump_csv(destination, header, data):
-	table = [ header ] + list(data.values())
+def dump_csv(destination, table):
 	with open(destination, mode='w', newline='', encoding='utf-8') as file:
 		csv.writer(file).writerows(table)
 
@@ -35,46 +34,92 @@ def fill_dict(path):
 
 	return { "data": data, "meta": meta }
 
+def get_po_files(po_paths):
+	failed = False
+
+	languages = {}
+	po_files = []
+	for po_path in po_paths:
+		if not po_path.is_dir():
+			failed = True
+			print(f"{po_path} not a folder")
+			continue
+
+		_po_files = {}
+		for f in po_path.iterdir():
+			if f.is_file() and str(f).endswith(".po"):
+				po_id = f.parts[-1][0:-3]
+				_po_files[po_id] = fill_dict(f)
+				if not po_id in languages:
+					languages[po_id] = _po_files[po_id]["meta"]["id"]
+				if languages[po_id] != _po_files[po_id]["meta"]["id"]:
+					failed = True
+					print(f"inconsistent language mapping {languages[po_id]} / {_po_files[po_id]["meta"]["id"]}")
+					break
+		if failed:
+			continue
+
+		if SOURCE_LANG not in _po_files:
+			failed = True
+			po_path = str(po_path / f"{SOURCE_LANG}.po")
+			print(f"{po_path} not found")
+			continue
+
+		po_files += [ _po_files ]
+
+	if not failed:
+		return {
+			"files": po_files,
+			"languages": sorted([ languages[k] for k in languages if k != SOURCE_LANG ])
+		}
+
+def build_matrix(languages, po_files):
+
+	matrix = {}
+
+	for files in po_files:
+		current = files[SOURCE_LANG]
+		_matrix = {}
+
+		for k in current["data"]:
+			if k in matrix:
+				print(f"Duplicate key {k}")
+			v = current["data"][k]
+			_matrix[k] = [
+				v["string"] if "string" in v else "",
+				k,
+				v["remarks"] if "remarks" in v else "",
+				v["filter"] if "filter" in v else ""
+			]
+
+		files = { files[f]["meta"]["id"]: files[f] for f in files if f != SOURCE_LANG }
+		files = [ files[f]["data"] if f in files else {} for f in languages ]
+
+		for k in _matrix:
+			for f in files:
+				_matrix[k] += [ f[k]["string"] if ( k in f and "string" in f[k] ) else "" ]
+			matrix[k] = _matrix[k]
+
+	return matrix
+
 def main(args):
-	if len(args) != 3:
+	if len(args) < 3:
 		return print(__doc__)
 
-	po_path = Path(args[1])
+	po_files = get_po_files([ Path(f) for f in args[1:-1] ])
 
-	if not po_path.is_dir():
+	if po_files is None:
 		return print(__doc__)
 
-	po_files = {}
-	for f in po_path.iterdir():
-		if f.is_file() and str(f).endswith(".po"):
-			po_files[f.parts[-1][0:-3]] = f
+	languages = po_files["languages"]
+	po_files = po_files["files"]
 
-	if SOURCE_LANG not in po_files:
-		po_path = str(po_path / f"${SOURCE_LANG}.po")
-		return print(f"{po_path} not found")
+	header = [ po_files[0][SOURCE_LANG]["meta"]["id"], "Identifier", "Remarks", "Filter" ] + languages
+	matrix = build_matrix(languages, po_files)
 
-	header = []
-	strings = {}
+	table = [ header ] + [ matrix[k] for k in sorted(matrix) ]
 
-	current=fill_dict(po_files[SOURCE_LANG])
-	header += [ current["meta"]["id"], "Identifier", "Remarks", "Filter" ]
-	for k in current["data"]:
-		v = current["data"][k]
-		strings[k] = [
-			v["string"] if "string" in v else "",
-			k,
-			v["remarks"] if "remarks" in v else "",
-			v["filter"] if "filter" in v else ""
-		]
-	del po_files[SOURCE_LANG]
-
-	for k in po_files:
-		current = fill_dict(po_files[k])
-		header += [ current["meta"]["id"] ]
-		for kk in strings:
-			strings[kk] += [ current["data"][kk]["string"] if kk in current["data"] and "string" in current["data"][kk] else "" ]
-
-	dump_csv(args[2], header, strings)
+	dump_csv(args[-1], table)
 
 if __name__ == "__main__":
     main(sys.argv)
