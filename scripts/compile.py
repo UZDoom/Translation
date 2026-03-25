25ca5731f0fca92d0a0de35ce3def5fbd34c8f33
Marcus Minhorst <marcus.minhorst@gmail.com>
2026-03-25 11:58:47 -0400

    Fix a thing for windows

--- scripts/compile.py
+++ scripts/compile.py
@@ -283,7 +283,7 @@ def main(args):
 
     if po_files is None:
         print(__doc__)
-        print(f"Available recipes: {" ".join(RECIPES.keys())}")
+        print("Available recipes: " + " ".join(RECIPES.keys()))
         sys.exit(1)
 
     languages = po_files["languages"]
