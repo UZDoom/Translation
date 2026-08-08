b4840d882feb5208436f9d7fb7c6fe7a137b21fc
Marcus Minhorst <marcus.minhorst@gmail.com>
2026-08-08 13:26:13 -0400

    Added some documentation

--- scripts/compile.py
+++ scripts/compile.py
@@ -60,14 +60,18 @@ def fill_dict(path):
     data = {}
 
     meta["id"] = po.metadata["Language"]
+
+    # used to alert something when wrong
     meta["valid"] = True
 
     # for now uzdoom needs the top left cell to be "default"
     if meta["id"] == SOURCE_LANG or meta["id"] == SOURCE_LANG_ALT:
         meta["id"] = "default"
 
+    # If we are using unifont instead of doom's font
     has_utf = meta["id"] in ["ja", "ko", "zh_Hans", "zh_Hant"]
 
+    # loop through each entry in a language, and add to map
     for e in po:
         specific_id = e.msgid
         entry = {"id": e.msgid}
@@ -77,6 +81,7 @@ def fill_dict(path):
         if e.tcomment:
             entry["remarks"] = e.tcomment
         if e.msgctxt:
+            # filters are used to change strings in certain cases
             entry["filter"] = e.msgctxt
             specific_id = f"{specific_id}#{e.msgctxt}"
 
@@ -107,14 +112,19 @@ def get_po_files(po_paths):
             continue
 
         _po_files = {}
+        # find each po file in the dir, add to dict using the filename
         for f in po_path.iterdir():
             if f.is_file() and str(f).endswith(".po"):
+
                 po_id = f.parts[-1][0:-3]
                 _po_files[po_id] = fill_dict(f)
+
                 if not _po_files[po_id]["meta"]["valid"]:
                     failed = True
+
                 if po_id not in languages:
                     languages[po_id] = _po_files[po_id]["meta"]["id"]
+
                 if languages[po_id] != _po_files[po_id]["meta"]["id"]:
                     failed = True
                     la = languages[po_id]
